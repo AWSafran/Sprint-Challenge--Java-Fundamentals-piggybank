80c8a0fa80f6f70cdee687f3a05fc6b895752a01
AWSafran <andrewsafran@gmail.com>
2019-04-26 11:12:11 -0400

    created java project in intelliJ

---
+++ src/com/piggyBank/Coin.java
@@ -0,0 +1,5 @@
+package src.com.piggyBank;
+
+public class Coin
+{
+}
---
+++ src/com/piggyBank/Main.java
@@ -0,0 +1,8 @@
+package com.piggyBank;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
